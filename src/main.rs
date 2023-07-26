f182c82477769f3f62d14a42f67d8645179b5df5
Julian Rosse <julian@helixbass.net>
2023-07-25 21:07:42 -0400

    don't deadlock

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ impl LanguageServer for Backend {
 
     async fn initialized(&self, _: InitializedParams) {
         // self.client
-        //     .log_message(MessageType::INFO, "server initialized!")
+        //     .log_message(tower_lsp::lsp_types::MessageType::INFO, "server initialized!")
         //     .await;
     }
 
@@ -98,41 +98,47 @@ impl LanguageServer for Backend {
     }
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
-        let mut file_state = self
-            .per_file
-            .get_mut(&params.text_document.uri)
-            .expect("Changed file wasn't loaded");
-        assert!(
-            params.content_changes.len() == 1,
-            "Only handling single content change currently"
-        );
-        let content_change = &params.content_changes[0];
-        match content_change.range {
-            Some(range) => {
-                let start_char = lsp_position_to_char_offset(&file_state.contents, range.start);
-                let end_char = lsp_position_to_char_offset(&file_state.contents, range.end);
-                let start_byte = file_state.contents.char_to_byte(start_char);
-                let old_end_byte = file_state.contents.char_to_byte(end_char);
-                file_state.contents.remove(start_char..end_char);
-                file_state.contents.insert(start_char, &content_change.text);
+        {
+            let mut file_state = self
+                .per_file
+                .get_mut(&params.text_document.uri)
+                .expect("Changed file wasn't loaded");
+            for content_change in &params.content_changes {
+                match content_change.range {
+                    Some(range) => {
+                        let start_char =
+                            lsp_position_to_char_offset(&file_state.contents, range.start);
+                        let end_char = lsp_position_to_char_offset(&file_state.contents, range.end);
+                        let start_byte = file_state.contents.char_to_byte(start_char);
+                        let old_end_byte = file_state.contents.char_to_byte(end_char);
+                        file_state.contents.remove(start_char..end_char);
+                        file_state.contents.insert(start_char, &content_change.text);
 
-                let new_end_byte = start_byte + content_change.text.len();
-                let input_edit = InputEdit {
-                    start_byte,
-                    old_end_byte,
-                    new_end_byte,
-                    start_position: position_to_point(range.start),
-                    old_end_position: position_to_point(range.end),
-                    new_end_position: byte_offset_to_point(&file_state.contents, new_end_byte),
-                };
-                file_state.tree.edit(&input_edit);
-                file_state.tree = parse(&file_state.contents, Some(&file_state.tree));
-            }
-            None => {
-                file_state.contents = (&*content_change.text).into();
-                file_state.tree = parse_from_scratch(&file_state.contents);
+                        let new_end_byte = start_byte + content_change.text.len();
+                        let input_edit = InputEdit {
+                            start_byte,
+                            old_end_byte,
+                            new_end_byte,
+                            start_position: position_to_point(range.start),
+                            old_end_position: position_to_point(range.end),
+                            new_end_position: byte_offset_to_point(
+                                &file_state.contents,
+                                new_end_byte,
+                            ),
+                        };
+                        file_state.tree.edit(&input_edit);
+                        file_state.tree = parse(&file_state.contents, Some(&file_state.tree));
+                    }
+                    None => {
+                        file_state.contents = (&*content_change.text).into();
+                        file_state.tree = parse_from_scratch(&file_state.contents);
+                    }
+                }
             }
         }
+
+        self.run_linting_and_report_diagnostics(&params.text_document.uri)
+            .await;
     }
 }
 
